dd44bcdb286e8e54233394d34762f7d6c5203ae6
chanhyeok3856 <grass3856@gmail.com>
2024-06-07 12:02:43 +0900

    0607

--- begin/src/main/java/min/boot/jpa/repository/DeptRepository.java
+++
@@ -1,8 +0,0 @@
-package min.boot.jpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import min.boot.jpa.entity.Dept;
-
-public interface DeptRepository extends JpaRepository<Dept, Integer>{
-}
