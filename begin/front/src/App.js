c38fe268419b657cadd2145d359edc9522871415
chanhyeok3856 <grass3856@gmail.com>
2024-06-13 17:51:05 +0900

    0613

--- begin/front/src/App.js
+++ begin/front/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>안녕</p>
-      </header>
-    </div>
-  );
+function App( ) {
+const [count, setCount] = useState(0);
+useEffect(( ) => {
+console.log('컴포넌트가 마운트되었습니다.');
+console.log(`현재 카운트 값: ${count}`);
+return ( ) => {
+console.log('컴포넌트가 언마운트되었습니다.');
+};
+}, [count]);
+return (
+<div className="App">
+<p>현재 카운터 수 : {count}</p>
+<button onClick={( ) => setCount(count + 1)}>카운트 증가</button>
+</div>
+);
 }
-
 export default App;
